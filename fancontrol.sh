6f8ce9e64456532f8f493af01705c924a8222de7
lburais <laurent@burais.fr>
2020-01-29 11:18:21 +0100

    Update fancontrol.sh

--- fancontrol.sh
+++ fancontrol.sh
@@ -1,6 +1,6 @@
 sudo gcc fancontrol.c -o fancontrol 
+sudo systemctl stop fancontrol
 sudo cp fancontrol /usr/bin
 sudo cp fancontrol.service /etc/systemd/system
 sudo systemctl enable fancontrol
 sudo systemctl start fancontrol
-
